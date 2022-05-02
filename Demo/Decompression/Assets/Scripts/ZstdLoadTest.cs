d8f07c1b3e207c4586b828f73c5cfaa0159d35d4
Andrii Popazov <andrey.plagiatix@gmail.com>
2022-05-02 12:42:00 +0300

    Decompression demo

---
+++ Demo/Decompression/Assets/Scripts/ZstdLoadTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Uzstd.Test
+{
+    public class ZstdLoadTest : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            var before   = Resources.Load<TextAsset>("txt/sample0").bytes; //圧縮前データ
+            var deflated = Resources.Load<TextAsset>("sample_deflated").bytes; //圧縮後データ
+            var dictdeflated = Resources.Load<TextAsset>("sample_dictdeflated").bytes; //辞書圧縮後データ
+            var dict = Resources.Load<TextAsset>("sample_dict").bytes;
+
+            Debug.Log(string.Format("    deflate before {0}byte after {1}byte", before.Length, deflated.Length));
+            Debug.Log(string.Format("dictdeflate before {0}byte after {1}byte", before.Length, dictdeflated.Length));
+
+            //simple decompress
+            {
+                var buffer = new byte[before.Length];
+                var result = API.decompress(buffer, before.Length, deflated, deflated.Length);
+                Debug.Log(string.Format("decompress result {0}", result));
+                CompareBuffer(before, buffer, "simple decompress buffer check");
+            }
+
+            //dictionary decompress
+            {
+                using (var decompressContext = DecompressContext.CreateWithDictionary(dict))
+                {
+                    var buffer = new byte[before.Length];
+                    var result = API.decompressDictionary(decompressContext, buffer, buffer.Length, deflated, deflated.Length);
+                    Debug.Log(string.Format("dictionary decompress result {0}", result));
+                    CompareBuffer(before, buffer, "dictionary decompress buffer check");
+                }
+            }
+
+            //stream decompress
+            using (var decompressContext = DecompressContext.CreateWithStream())
+            {
+                var buffer = new byte[before.Length];
+                var result = API.decompressStream(decompressContext, buffer, buffer.Length, deflated, deflated.Length);
+                Debug.Log(string.Format("streaming decompress result {0}", result));
+                CompareBuffer(before, buffer, "streaming decompress buffer check");
+            }
+
+            //stream decompress using work buffer
+            using (var decompressContext = DecompressContext.CreateWithStream())
+            {
+                var buffer = new byte[before.Length];
+                int totalDecompressed = 0;
+                var work = new byte[buffer.Length / 4];
+                do
+                {
+                    var result = API.decompressStream(decompressContext, work, work.Length, deflated, deflated.Length);
+                    if (result == 0) { break; } // success decompress all.
+                    if (result < 0) { throw new System.Exception("decompress failed"); }
+
+                    System.Array.Copy(work, 0, buffer, totalDecompressed, result);
+                    totalDecompressed += result;
+
+                } while (totalDecompressed < buffer.Length);
+                CompareBuffer(before, buffer, "streaming decompress2 buffer check");
+            }
+
+            //stream decompress with dictionary
+            using (var decompressContext = DecompressContext.CreateWithStreamDictionary(dict))
+            {
+                var buffer = new byte[before.Length];
+                var result = API.decompressStreamDictionary(decompressContext, buffer, buffer.Length, deflated, deflated.Length);
+                Debug.Log(string.Format("streaming decompress result {0}", result));
+                CompareBuffer(before, buffer, "stream dictionary decompress buffer check");
+            }
+        }
+
+        private void CompareBuffer(byte[] first, byte[] second, string msg)
+        {
+            var compare = Enumerable.SequenceEqual(first, second);
+            Debug.Log(string.Format("{0}. {1}", msg, compare));
+        }
+    }
+}
---
+++ Demo/Decompression/Assets/Uzstd/Uzstd.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Uzstd
+{
+    public static class API
+    {
+#if (UNITY_IPHONE || UNITY_WEBGL || UNITY_SWITCH) && !UNITY_EDITOR
+        const string ZSTDDLL = "__Internal";
+#else
+        const string ZSTDDLL = "zstd";
+#endif
+
+        /// <summary>
+        /// decompress zstd
+        /// return 1 is error. 0 is success.
+        /// </summary>
+        /// <example>
+        /// byte[] src;
+        /// byte[] dst;
+        /// var result = Uzstd.decompress(dst, dst.Length, src, src.Length);
+        /// </example>
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int decompress([Out] byte[] dst, int dstSize, [In] byte[] src, int srcSize);
+
+        /// <summary>
+        /// decompress zstd with dictionary.
+        /// return 1. decompress error.
+        /// </summary>
+        /// <example>
+        /// byte[] src;
+        /// byte[] dst;
+        /// byte[] dict;
+        /// using(var context = DecompressContext.CreateDictionary(dict, dict.Length))
+        /// {
+        ///   var result = Uzstd.decompressDictionary(context, dst, dst.Length, src, src.Length);
+        /// }
+        /// </example>
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int decompressDictionary([In][Out] DecompressContext context, [Out] byte[] dst, int dstSize, [In] byte[] src, int srcSize);
+
+        /// <summary>
+        /// streaming decompress zstd
+        /// return > 0. decompressed size
+        /// return = 0. done decompress.
+        /// return < 0. error.
+        /// </summary>
+        /// <example>
+        /// using(var context = DecompressContext.Create()) //DecompressContext.CreateStreamDictionary(dict, dict.Length)
+        /// {
+        ///   byte[] compressed;
+        ///   byte[] decompressed;
+        ///   byte[] work; //it can be smaller than decompressed.
+        ///   int totalDecompressedSize = 0;
+        ///   do
+        ///   {
+        ///      var decompressedSize = decompressStream(context, work, work.Length, compressed, compressed.Length );
+        ///      if( decompressedSize == 0 ) { break; } //success to read.
+        ///      if( decompressedSize < 0 ) { throw new System.Exception("..."); }
+        ///      System.Array.Copy( work, 0, decompressed, totalDecompressedSize, decompressedSize );
+        ///      totalDecompressedSize += decompressedSize;
+        ///   } while (totalDecompressedSize < decompressed.Length);
+        /// }
+        /// </example>
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int decompressStream([In][Out] DecompressContext context, [Out] byte[] dst, int dstSize, [In] byte[] src, int srcSize);
+
+        /// <summary>
+        /// streaming decompress zstd (with dictionary)
+        /// <see cref="decompressStream(DecompressContext, byte[], int, byte[], int)"/>
+        /// </summary>
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int decompressStreamDictionary([In][Out] DecompressContext context, [Out] byte[] dst, int dstSize, [In] byte[] src, int srcSize);
+    }
+
+    /// <summary>
+    /// streaming decompress informations.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public class DecompressContext : IDisposable
+    {
+#if (UNITY_IPHONE || UNITY_WEBGL || UNITY_SWITCH) && !UNITY_EDITOR
+        const string ZSTDDLL = "__Internal";
+#else
+        const string ZSTDDLL = "zstd";
+#endif
+
+        /// <summary>
+        /// create for stream.
+        /// </summary>
+        public static DecompressContext CreateWithStream()
+        {
+            var context = new DecompressContext();
+            initializeDecompressContext(context);
+            return context;
+        }
+
+        /// <summary>
+        /// create for dictionary.
+        /// </summary>
+        public static DecompressContext CreateWithDictionary(byte[] dict)
+        {
+            var context = new DecompressContext();
+            initializeDecompressContextDictionary(context, dict, dict.Length);
+            return context;
+        }
+
+        /// <summary>
+        /// create for stream with dictionary.
+        /// </summary>
+        public static DecompressContext CreateWithStreamDictionary(byte[] dict)
+        {
+            var context = new DecompressContext();
+            initializeDecompressContextStreamDictionary(context, dict, dict.Length);
+            return context;
+        }
+
+        public IntPtr dctx;
+        public IntPtr ddict;
+        public IntPtr dstream;
+        public int    totalReadSize;
+
+        public void Dispose()
+        {
+            finalizeDecompressContext(this);
+        }
+
+        DecompressContext()
+        {
+            this.dctx = default(IntPtr);
+            this.ddict = default(IntPtr);
+            this.dstream = default(IntPtr);
+            this.totalReadSize = 0;
+        }
+
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void initializeDecompressContext([In][Out] DecompressContext context);
+
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void initializeDecompressContextDictionary([In][Out] DecompressContext context, [In] byte[] dict, int dictSize);
+
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void initializeDecompressContextStreamDictionary([In][Out] DecompressContext context, [In] byte[] dict, int dictSize);
+
+        [DllImport(ZSTDDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void finalizeDecompressContext([In][Out] DecompressContext context);
+    }
+}
